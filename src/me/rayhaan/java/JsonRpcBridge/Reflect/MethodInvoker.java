046e681d8da463973f744743ba503e40487fe5c9
Rayhaan Jaufeerally <rayhaan@rayhaan.me>
2013-11-21 16:13:30 +0000

    Created MethodInvoker to allow more dynamic resolving of correct methods.

---
+++ src/me/rayhaan/java/JsonRpcBridge/Reflect/MethodInvoker.java
@@ -0,0 +1,14 @@
+package me.rayhaan.java.JsonRpcBridge.Reflect;
+
+public class MethodInvoker {
+
+	private Class<?> clazz;
+	
+	
+	public MethodInvoker(Class<?> clazz) {
+		this.clazz = clazz;
+	}
+	
+	
+	
+}
